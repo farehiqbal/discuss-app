1573e8752c27ed0878e8a76ee80c2f867953a739
Fareh Iqbal <fareh.legend@gmail.com>
2024-08-21 09:45:22 +0500

    validations with zod on topics
    Completed topics side with validations in zod along with cache handlings.

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -2,16 +2,24 @@
 
 import { create } from 'domain';
 import {z} from 'zod';
+import {auth } from '@/auth';
+import type { Topic } from '@prisma/client';
+import { redirect } from 'next/navigation';
+import { db } from '@/db';
+import paths from '@/paths';
+import { revalidatePath } from 'next/cache';
+
 
 const createTopicSchema = z.object({
     name: z.string().min(3).regex(/^[a-z-]+$/, {message: 'Name must be lowercase letters or dashes without spaces'}),
-    description: z.string().max(10),
+    description: z.string().min(10),
 });
 
 interface createTopicFormState{
     errors: {
         name?: string[];
         description?: string[];
+        _form ?: string[];
     }
 }
 
@@ -30,10 +38,39 @@ export async function createTopic(
             errors: result.error.flatten().fieldErrors
        };
     }
-
-    return {
-        errors: {}
+    const session = await auth();
+    if (!session || !session.user) {
+        return {
+            errors: {
+                _form: ['You must be signed in to create a topic']
+            },
+        };
     }
+    let topic: Topic;
+    try {
+        topic = await db.topic.create({
+            data: {
+                slug: result.data.name,
+                description: result.data.description,
+            }
+        });
 
-    // TODO: revalidate the homepage
-}
+    } catch (err: unknown)  {
+        if (err instanceof Error ) {
+            return {
+                errors: {
+                    _form: [err.message]
+                }
+            }
+        } else {
+            return {
+                errors: {
+                    _form: ['Something went wrong']
+                }
+            }
+        }
+    }
+    revalidatePath('/');
+    redirect(paths.topicShow(topic.slug));
+    
+}   
--- src/components/topics/topic-create-form.tsx
+++ src/components/topics/topic-create-form.tsx
@@ -25,13 +25,25 @@ export default function TopicCreateForm() {
                 <form action={action} >
                     <div className="flex flex-col gap-4 p-4 w-80">
                         <h3 className='text-lg'>Create a Topic</h3>
-                        <Input name='name' label="Name" labelPlacement='outside' placeholder='Name' ></Input>
+                        <Input name='name' label="Name" labelPlacement='outside' placeholder='Name' 
+                            isInvalid={!!formState.errors.name}
+                            errorMessage={formState.errors.name?.join(', ')}
+                        />
                         <Textarea 
                             name='description'
                             label="Description" 
                             labelPlacement='outside' 
-                            placeholder='Descibe your topic' 
+                            placeholder='Descibe your topic'
+                            isInvalid={!!formState.errors.description}
+                            errorMessage={formState.errors.description?.join(', ')} 
                         />
+
+                        {formState.errors._form? (
+                            <div className='rounded p-2 bg-red-200 border border-red-400'>
+                                {formState.errors._form.join(', ')}
+                            </div>
+                        ) : null}
+
                         <Button type='submit'>Submit</Button>
                     </div>
                 </form>
