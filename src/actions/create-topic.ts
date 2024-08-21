514d99e16dfc77431db1c4d7a1442524536964c8
Fareh Iqbal <fareh.legend@gmail.com>
2024-08-21 09:10:07 +0500

    Added formstate Type

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,6 +1,39 @@
 'use server'
 
-export async function createTopic() {
+import { create } from 'domain';
+import {z} from 'zod';
+
+const createTopicSchema = z.object({
+    name: z.string().min(3).regex(/^[a-z-]+$/, {message: 'Name must be lowercase letters or dashes without spaces'}),
+    description: z.string().max(10),
+});
+
+interface createTopicFormState{
+    errors: {
+        name?: string[];
+        description?: string[];
+    }
+}
+
+export async function createTopic(
+    formState: createTopicFormState , 
+    formData: FormData
+): Promise<createTopicFormState> {
+ 
+   const result = createTopicSchema.safeParse({
+        name : formData.get('name'),
+        description: formData.get('description'),
+   });
+
+   if (!result.success) {
+       return {
+            errors: result.error.flatten().fieldErrors
+       };
+    }
+
+    return {
+        errors: {}
+    }
 
     // TODO: revalidate the homepage
 }
