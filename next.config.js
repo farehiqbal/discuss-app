0593b458fe7a2c2344989b2515f60cb307fff442
farehiqbal <fareh.iqbal55@gmail.com>
2024-10-08 03:57:06 +0500

    fix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
+// const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 module.exports = {
   webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
-    }
+    // if (isServer) {
+    //   config.plugins = [...config.plugins, new PrismaPlugin()];
+    // }
 
     return config;
   },
