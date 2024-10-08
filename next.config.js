d6b28f3db000889077de772639c9ff94ff68a247
farehiqbal <fareh.iqbal55@gmail.com>
2024-10-08 13:46:24 +0500

    images fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 const { PrismaPlugin } = require('@prisma/nextjs-monorepo-workaround-plugin');
 
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+  
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.plugins = [...config.plugins, new PrismaPlugin()];
